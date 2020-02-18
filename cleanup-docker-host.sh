5447fd4d1f9afc2abe3d10dd4313c293745982b2
Hyungu Cho <hgcho@bluedigm.com>
2020-02-18 16:24:13 +0900

    Add cleanup-docker-host.sh

---
+++ cleanup-docker-host.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "- Run garbage collection for docker volume"
+yes | docker volume prune | echo -ne
+
+echo "- Run garbage collection for docker system(unused images, networks, etc.)"
+yes | docker system prune | echo -ne
+
+echo "Done."
+
